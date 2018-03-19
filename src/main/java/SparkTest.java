0b9f93c5fe96aa4945b886c524cf0c6409c4642f
songc <songcmail@gmail.com>
2018-03-19 22:22:24 +0800

    modify the pom, add plugin to build jar with dependency, and optimize imports.

--- src/main/java/SparkTest.java
+++ src/main/java/SparkTest.java
@@ -1,9 +1,7 @@
 import com.alibaba.fastjson.JSON;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
-import org.apache.hadoop.hbase.TableName;
-import org.apache.hadoop.hbase.client.*;
-import org.apache.hadoop.hbase.filter.KeyOnlyFilter;
+import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
 import org.apache.hadoop.hbase.mapreduce.TableInputFormat;
 import org.apache.hadoop.hbase.util.Bytes;
@@ -12,11 +10,10 @@ import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
-import java.awt.image.BufferedImage;
-import java.io.IOException;
 import java.io.Serializable;
 import java.util.List;
 import java.util.stream.LongStream;
+
 
 
 /**
@@ -29,23 +26,18 @@ public class SparkTest implements Serializable{
 
     private String family="info";
 
-    private byte[] qParentId = Bytes.toBytes("parentId");
-    private byte[] qName = Bytes.toBytes("name");
     private byte[] qContent = Bytes.toBytes("content");
 
     public static void main(String[] args) {
         SparkTest sparkTest = new SparkTest();
         String appName = "test";
         String master = "spark://hadoop-hbase:7077";
-        SparkConf conf = new SparkConf().setAppName(appName).setMaster("local");
+        SparkConf conf = new SparkConf().setAppName(appName).setMaster(master);
         JavaSparkContext sc = new JavaSparkContext(conf);
-
-        Configuration config;
-        config = HBaseConfiguration.create();
+        Configuration config= HBaseConfiguration.create();
         config.set("hbase.roodir","hdfs://hadoop-hbase:9000/hbase");
         config.set("hbase.zookeeper.quorum","hadoop-hbase");
         config.set("hbase.zookeeper.property.clientPort", "2181");
-        config.set("hbase.client.keyvalue.maxsize", "524288000");
         config.set(TableInputFormat.INPUT_TABLE, sparkTest.tableName);
         config.set(TableInputFormat.SCAN_COLUMN_FAMILY, sparkTest.family);
         JavaPairRDD<ImmutableBytesWritable, Result> hbaseRDD = sc.newAPIHadoopRDD(config, TableInputFormat.class,
@@ -53,15 +45,13 @@ public class SparkTest implements Serializable{
         List<double[]> result = hbaseRDD.map(tuple2 -> {
             String rowKey = new String(tuple2._2.getRow());
             TiffImage image = new TiffImage(tuple2._2.getValue(sparkTest.family.getBytes(), sparkTest.qContent), rowKey);
-            return image.getAllRegionGrayAverage(400,400);
+            return image.getAllRegionGrayAverage(50,50);
         }).collect();
         JavaRDD<double[]> f = sc.parallelize(ImageUtil.getTranspose(result));
         f.map(s->{
             double[] x = LongStream.rangeClosed(1, s.length).asDoubleStream().toArray();
-            return ExponentFitUtil.getDualExpFuncValue(SignalFit.fitDualExponent(x,s),x);
-        }).map(JSON::toJSONString).saveAsTextFile("result2");
-
-
+            return ExponentFitUtil.getOneExpFuncValue(SignalFit.fitOneExponent(x,s),x);
+        }).map(JSON::toJSONString).saveAsTextFile("/home/hadoop/result");
     }
 
 }
